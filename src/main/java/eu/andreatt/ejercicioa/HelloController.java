9c07ffe09122a3f319e488f1d94d8eec1fabfb20
andreatticj <andreatticj@gmail.com>
2024-10-04 10:56:37 +0200

    CHANGE en ejercicioA.fxml y empezar a hacer cosas en HelloController.java

--- src/main/java/eu/andreatt/ejercicioa/HelloController.java
+++ src/main/java/eu/andreatt/ejercicioa/HelloController.java
@@ -6,14 +6,11 @@ package eu.andreatt.ejercicioa;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class HelloController {
 
@@ -28,6 +25,16 @@ public class HelloController {
 
     @FXML // fx:id="comboEdades"
     private ComboBox<String> comboEdades; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioHombre"
+    private RadioButton radioHombre; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioMujer"
+    private RadioButton radioMujer; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioOtro"
+    private RadioButton radioOtro; // Value injected by FXMLLoader
+
 
     @FXML // fx:id="grpSexo"
     private ToggleGroup grpSexo; // Value injected by FXMLLoader
@@ -51,6 +58,42 @@ public class HelloController {
     private TextField txtProfesion; // Value injected by FXMLLoader
 
 
+    @FXML
+    void aceptar(ActionEvent event) {
+
+    }
+
+    private void mostrarAlertInfo(Window win) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initOwner(win);
+        alert.setHeaderText(null);
+        alert.setTitle("TUS DATOS");
+        String profesion = "Profesión: "+txtProfesion.getText();
+
+        alert.setContentText("Información sobre la aplicación");
+        alert.showAndWait();
+    }
+
+    private void verificarInfo(){
+        String errores = "";
+        if (txtProfesion.getText().isEmpty()){
+            errores += "Hay que rellenar el campo profesion.\n";
+        }
+        if (txtHermanos.getText().isEmpty()){
+            errores += "Hay que rellenar el campo Nº hermanos.\n";
+        }
+        if (!radioHombre.isSelected() && !radioMujer.isSelected() && !radioOtro.isSelected()){
+            errores += "Hay que seleccionar un sexo.\n";
+        }
+        if (chkDeporte.isSelected() && listaDeportes.getSelectionModel().getSelectedItems().isEmpty()){
+            errores += "Tienes que indicar los deportes que practicas";
+        }
+    }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
 
 
     @FXML
@@ -61,6 +104,10 @@ public class HelloController {
 
         ObservableList<String> elementosLista = FXCollections.observableArrayList("Tenis", "Fútbol","Baloncesto","Natación","Ciclismo","Otro");
         listaDeportes.setItems(elementosLista);
+
+        if (chkDeporte.isSelected()){
+            listaDeportes.setDisable(true);
+        }
     }
 
 }
